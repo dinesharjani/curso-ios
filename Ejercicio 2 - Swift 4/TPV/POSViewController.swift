50bb522364e934d4166430a6a7c4a144a74cd756
Dinesh Harjani <dinesharjani@gmail.com>
2018-03-05 11:33:42 +0000

    POSViewController now uses Property Observers.

--- Ejercicio 2 - Swift 4/TPV/POSViewController.swift
+++ Ejercicio 2 - Swift 4/TPV/POSViewController.swift
@@ -12,32 +12,39 @@ class POSViewController: UIViewController {
     
     @IBOutlet var orderLabels: [UILabel]!
     
-    var hamburguesas: Int = 0
-    var papasFritas: Int = 0
-    var helados: Int = 0
+    var hamburguesas: Int = 0 {
+        didSet {
+            orderLabels[0].text = "Hamburguesas: \(hamburguesas)"
+        }
+    }
+    
+    var papasFritas: Int = 0 {
+        didSet {
+            orderLabels[1].text = "Papas Fritas: \(papasFritas)"
+        }
+    }
+    var helados: Int = 0 {
+        didSet {
+            orderLabels[2].text = "Helados: \(helados)"
+        }
+    }
     
     @IBAction func buttonWasTapped(_ sender: UIButton) {
         hamburguesas += 1
-        orderLabels[0].text = "Hamburguesas: \(hamburguesas)"
     }
     
     @IBAction func papasFritasWasTapped(_ sender: UIButton) {
         papasFritas += 1
-        orderLabels[1].text = "Papas Fritas: \(papasFritas)"
     }
     
     @IBAction func heladosWasTapped(_ sender: UIButton) {
         helados += 1
-        orderLabels[2].text = "Helados: \(helados)"
     }
     
     @IBAction func newOrderWasTapped(_ sender: UIButton) {
         hamburguesas = 0
-        orderLabels[0].text = "Hamburguesas: \(hamburguesas)"
         papasFritas = 0
-        orderLabels[1].text = "Papas Fritas: \(papasFritas)"
         helados = 0
-        orderLabels[2].text = "Helados: \(helados)"
     }
     
     override func viewDidLoad() {
