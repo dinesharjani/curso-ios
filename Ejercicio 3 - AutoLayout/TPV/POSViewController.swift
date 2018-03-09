f88189e3b2284f7069972b794bab59f3e3d2207b
Dinesh Harjani <dinesharjani@gmail.com>
2018-03-09 16:15:37 +0000

    Exercise 3: Added a background color to POSViewController.

--- Ejercicio 3 - AutoLayout/TPV/POSViewController.swift
+++ Ejercicio 3 - AutoLayout/TPV/POSViewController.swift
@@ -79,6 +79,8 @@ class POSViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1)
     }
 }
 
