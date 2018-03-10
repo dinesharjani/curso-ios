927271ffb777d2ef8e1aa8a004f20701005d585f
Dinesh Harjani <dinesharjani@gmail.com>
2018-03-10 12:19:02 +0000

    Exercise 4: We now have a segue to move to the new OrderSummaryViewController.

--- Ejercicio 4 - Navegación/TPV/OrderSummaryViewController.swift
+++ Ejercicio 4 - Navegación/TPV/OrderSummaryViewController.swift
@@ -20,17 +20,10 @@ class OrderSummaryViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(onViewControllerTapped(_:))))
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @objc func onViewControllerTapped(_ sender: Any?) {
+        dismiss(animated: true, completion: nil)
     }
-    */
-
 }
