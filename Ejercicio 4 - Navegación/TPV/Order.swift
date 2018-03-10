401e98ba099bc657914b9a10fbce36d31ac1b19a
Dinesh Harjani <dinesharjani@gmail.com>
2018-03-10 10:40:47 +0000

    Added Exercise 3 as the starting point for Exercise 4.

---
+++ Ejercicio 4 - Navegación/TPV/Order.swift
@@ -0,0 +1,64 @@
+//
+//  Order.swift
+//  TPV
+//
+//  Created by Dinesh Harjani on 05/03/2018.
+//  Copyright © 2018 Dinesh Harjani. All rights reserved.
+//
+
+import Foundation
+
+class Order: Consumable {
+    
+    private class Item: Consumable {
+        
+        fileprivate enum Food: Consumable {
+            case Hamburger
+            case Fries
+            case IceCream
+            
+            var price: Price {
+                switch self {
+                case .Hamburger:
+                    return Price(units: 3, cents: 99)
+                case .Fries:
+                    return Price(units: 2, cents: 49)
+                case .IceCream:
+                    return Price(units: 1, cents: 89)
+                }
+            }
+        }
+        
+        let units: Int
+        let type: Food
+        
+        var price: Price {
+            return Price(type.price, multiplier: units)
+        }
+        
+        init(WithUnits units: Int, andType type: Food) {
+            self.units = units
+            self.type = type
+        }
+    }
+    
+    private var items = Array<Order.Item>()
+    
+    var price: Price {
+        var accumuledPrice = Price()
+        for item in items {
+            accumuledPrice += item.price
+        }
+        return accumuledPrice
+    }
+    
+    init?(hamburgers: Int, fries: Int, iceCream: Int) {
+        guard hamburgers + fries + iceCream > 0 else {
+            return nil
+        }
+        
+        self.items.append(Order.Item(WithUnits: hamburgers, andType: .Hamburger))
+        self.items.append(Order.Item(WithUnits: fries, andType: .Fries))
+        self.items.append(Order.Item(WithUnits: iceCream, andType: .IceCream))
+    }
+}
