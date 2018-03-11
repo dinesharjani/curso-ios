c0d08609ec2da75e846bff0fc90b33cabbbda16d
Dinesh Harjani <dinesharjani@gmail.com>
2018-03-11 12:35:37 +0000

    Exercise 5: Added Exercise 4 as the starting point for Exercise 5.

---
+++ Ejercicio 5 - Listas (UITableViewController)/TPV/OrderSummaryViewController.swift
@@ -0,0 +1,36 @@
+//
+//  OrderSummaryViewController.swift
+//  TPV
+//
+//  Created by Dinesh Harjani on 10/03/2018.
+//  Copyright © 2018 Dinesh Harjani. All rights reserved.
+//
+
+import UIKit
+
+class OrderSummaryViewController: UIViewController {
+
+    @IBOutlet weak var summaryLabel: UILabel! {
+        didSet {
+            guard order != nil else {
+                summaryLabel.text = "Total: 0.0€"
+                return
+            }
+
+            summaryLabel.text = "Total: \(order!.price)"
+        }
+    }
+    
+    public var order: Order?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Resumen"
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(onViewControllerTapped(_:))))
+    }
+    
+    @objc func onViewControllerTapped(_ sender: Any?) {
+        navigationController?.popViewController(animated: true)
+    }
+}
