8e7edce5e419ceb4f0c4de9e02fd1718ca736563
Dinesh Harjani <dinesharjani@gmail.com>
2018-03-12 11:57:07 +0000

    Exercise 6: Added POSButton class and nib file.

---
+++ Ejercicio 6 - UIView(s) Personalizadas/TPV/POSButton.swift
@@ -0,0 +1,15 @@
+//
+//  POSButton.swift
+//  TPV
+//
+//  Created by Dinesh Harjani on 12/03/2018.
+//  Copyright © 2018 Dinesh Harjani. All rights reserved.
+//
+
+import UIKit
+
+class POSButton: UIView {
+
+    
+
+}
