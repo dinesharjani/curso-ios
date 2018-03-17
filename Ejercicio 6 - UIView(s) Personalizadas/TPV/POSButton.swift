9833bf52023245e84b11032554daba6aad95c679
Dinesh Harjani <dinesharjani@gmail.com>
2018-03-17 19:40:08 +0000

    Exercise 6: Added a units property to POSButton, initialised to zero.

--- Ejercicio 6 - UIView(s) Personalizadas/TPV/POSButton.swift
+++ Ejercicio 6 - UIView(s) Personalizadas/TPV/POSButton.swift
@@ -12,9 +12,19 @@ class POSButton: UIView {
 
     @IBOutlet var contentView: UIView!
     
-    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var counterLabel: UILabel! {
+        didSet {
+            units = 0
+        }
+    }
     
     @IBOutlet weak var mainLabel: UILabel!
+    
+    private (set) var units = 0 {
+        didSet {
+            counterLabel.text = "\(units)"
+        }
+    }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
