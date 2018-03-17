604dcefd1d2e576b7bbdae0cc7a8e383c5505872
Dinesh Harjani <dinesharjani@gmail.com>
2018-03-17 19:33:03 +0000

    Exercise 6: We now have 3 simple POSButton(s) in POSViewController, instanced through their nib file.
    Unfortunately, they don't do much yet.

--- Ejercicio 6 - UIView(s) Personalizadas/TPV/POSButton.swift
+++ Ejercicio 6 - UIView(s) Personalizadas/TPV/POSButton.swift
@@ -10,8 +10,29 @@ import UIKit
 
 class POSButton: UIView {
 
+    @IBOutlet var contentView: UIView!
+    
     @IBOutlet weak var counterLabel: UILabel!
     
     @IBOutlet weak var mainLabel: UILabel!
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    func setup() {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: "POSButton", bundle: bundle)
+        nib.instantiate(withOwner: self, options: nil)
+        contentView.frame = bounds
+        contentView.autoresizingMask = [UIViewAutoresizing.flexibleWidth, .flexibleHeight]
+        
+        addSubview(contentView)
+    }
 }
